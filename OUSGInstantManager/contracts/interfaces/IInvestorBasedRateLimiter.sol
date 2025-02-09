5ea67c317762133751672e44bd9e87b260832f7f
Alexander Reed <117864716+amr080@users.noreply.github.com>
2025-02-09 01:43:44 +0000

    X

---
+++ OUSGInstantManager/contracts/interfaces/IInvestorBasedRateLimiter.sol
@@ -0,0 +1,42 @@
+/**SPDX-License-Identifier: BUSL-1.1
+
+      ▄▄█████████▄
+   ╓██▀└ ,╓▄▄▄, '▀██▄
+  ██▀ ▄██▀▀╙╙▀▀██▄ └██µ           ,,       ,,      ,     ,,,            ,,,
+ ██ ,██¬ ▄████▄  ▀█▄ ╙█▄      ▄███▀▀███▄   ███▄    ██  ███▀▀▀███▄    ▄███▀▀███,
+██  ██ ╒█▀'   ╙█▌ ╙█▌ ██     ▐██      ███  █████,  ██  ██▌    └██▌  ██▌     └██▌
+██ ▐█▌ ██      ╟█  █▌ ╟█     ██▌      ▐██  ██ └███ ██  ██▌     ╟██ j██       ╟██
+╟█  ██ ╙██    ▄█▀ ▐█▌ ██     ╙██      ██▌  ██   ╙████  ██▌    ▄██▀  ██▌     ,██▀
+ ██ "██, ╙▀▀███████████⌐      ╙████████▀   ██     ╙██  ███████▀▀     ╙███████▀`
+  ██▄ ╙▀██▄▄▄▄▄,,,                ¬─                                    '─¬
+   ╙▀██▄ '╙╙╙▀▀▀▀▀▀▀▀
+      ╙▀▀██████R⌐
+
+ */
+pragma solidity 0.8.16;
+
+interface IInvestorBasedRateLimiter {
+  /**
+   * @notice Rate limits mints for the provided investor
+   *
+   * @param investorAddress Address of the investor
+   * @param mintAmount Amount of token to be minted
+   *
+   */
+  function checkAndUpdateMintLimit(
+    address investorAddress,
+    uint256 mintAmount
+  ) external;
+
+  /**
+   * @notice Rate limits redemptions for the provided investor
+   *
+   * @param investorAddress Address of the investor
+   * @param redemptionAmount Amount of token to be redeemed
+   *
+   */
+  function checkAndUpdateRedemptionLimit(
+    address investorAddress,
+    uint256 redemptionAmount
+  ) external;
+}
