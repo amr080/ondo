262a04f7738b5fb37e6affbaf97b307f892c9737
Alexander Reed <117864716+amr080@users.noreply.github.com>
2025-02-09 05:14:22 +0000

    fix: deployment initialization and routing

--- fix-head.js
+++ fix-head.js
@@ -1,7 +1,3 @@
-(function() {
-  if (typeof window !== 'undefined') {
-    window.document = window.document || {};
-    window.document.head = window.document.head || document.getElementsByTagName('head')[0];
-    window.__next_f = window.__next_f || [];
-  }
-})();
+window.document = window.document || {};
+window.document.head = window.document.head || document.getElementsByTagName("head")[0];
+window.__next_f = window.__next_f || [];
